0d2ea9a73e9630132b27b908e5db9d7d60e0de9e
philipfweiss <weissp68@stanford.edu>
2016-06-15 15:22:35 -0700

    Euler24 boilerplate

---
+++ euler24.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+/* ********************************************
+ Use this command to compile the program:
+    $ g++ -Wall euler24.cpp -o euler24
+
+ And this command to run it after compulation:
+    $ ./euler24
+ ********************************************/
+
+
+int main () {
+
+  return 0;
+}
